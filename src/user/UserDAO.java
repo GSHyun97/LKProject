7b780f0d24cd85ffe94a60b1e49b41b4639fe708
Kang <Kang@14.33.9.56>
2021-09-27 15:28:12 +0900

    qwrqwe

--- src/user/UserDAO.java
+++ src/user/UserDAO.java
@@ -9,7 +9,7 @@ import java.sql.ResultSet;
 public class UserDAO {
 	
 	private Connection conn; 											//DB에 접근하게 해주는 객체
-	private PreparedStatement pstmt;  									//SQL문장 삽입할거임
+	private PreparedStatement pstmt;  									//SQL문장 삽입할거임ㄴㅇㅁ
 	private ResultSet rs;    											//정보를 담을 객체
 
 	public UserDAO() {         											//이 클래스가 생성될 때 DB와 연결
@@ -24,7 +24,7 @@ public class UserDAO {
 		}
 	}
 	
-	public int login(String userID, String userPassword) {					//로그인용
+	public int login (String userID, String userPassword) {					//로그인용
 		String SQL="SELECT user_Password FROM USER WHERE user_ID = ?";      //보안 때문에 ?를 사용하고 추후에 ? 위치에 특정값이 들어감, 입력한 ID의 userPassword를 가져옴
 		try {
 			pstmt=conn.prepareStatement(SQL);      							//SQL에 있는 문장 가져와서 넣을거임
@@ -32,7 +32,7 @@ public class UserDAO {
 			rs=pstmt.executeQuery();                   						//SQL에 문장 넣은 결과값을 저장
 			if(rs.next()) {                               					//결과가 존재한다면
 				if(rs.getString(1).equals(userPassword)) {    				//가져온 password가 입력한 password와 같으면
-					return 1;               								//로그인성공
+					return 1;             								//로그인성공
 				}
 			}
 			return -1;                      								//아이디가 없음
@@ -42,29 +42,54 @@ public class UserDAO {
 		return -2; //DB오류를 의미
 	}
 	//회원가입 메소드
-	public int addMember(dbgetset user) {					
+	public int register(String user_Id, String user_Password, String user_Name, String user_Birth1, String user_Birth2, String user_Birth3, String user_Email1, String user_Email2) {
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
 		String SQL="INSERT INTO USER VALUES (?, ?, ?, ?, ?, CURRENT_TIMESTAMP)";     
 		try {
-			pstmt=conn.prepareStatement(SQL);      							
-			pstmt.setString(1, user.getUser_Id());   
-			pstmt.setString(2, user.getUser_Password());
-			pstmt.setString(3, user.getUser_Name());
-			pstmt.setString(4, user.getUser_Birth1()+user.getUser_Birth2()+user.getUser_Birth3());
-			pstmt.setString(5, user.getUser_Email1()+"@"+user.getUser_Email2());
+			pstmt = conn.prepareStatement(SQL);      							
+			pstmt.setString(1, user_Id);   
+			pstmt.setString(2, user_Password);
+			pstmt.setString(3, user_Name);
+			pstmt.setString(4, user_Birth1+user_Birth2+user_Birth3);
+			pstmt.setString(5, user_Email1+"@"+user_Email2);
 			return pstmt.executeUpdate();                   				
 		}catch (Exception e) {
 			e.printStackTrace();
+		}finally {
+			try {
+				if(rs !=null) rs.close();
+				if(pstmt !=null) pstmt.close();
+		}catch(Exception e){
+			e.printStackTrace();
+			}
 		}
-		return -1; //DB오류를 의미
+		return -1;
+	}
+	//중복확인 메서드
+	public int registerCheck(String user_Id) {
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		String SQL = "SELECT * FROM USER WHERE user_ID = ?";
+		try {
+			pstmt=conn.prepareStatement(SQL);
+			pstmt.setString(1,  user_Id);
+			rs = pstmt.executeQuery();
+			if(rs.next() || user_Id.equals("")) {
+				return 0;
+			}else {
+				return 1;
+			}
+		}catch(Exception e){
+			e.printStackTrace();
+		}finally {
+			try {
+				if(rs !=null) rs.close();
+				if(pstmt !=null) pstmt.close();
+		}catch(Exception e){
+			e.printStackTrace();
+			}
+		}
+		return -1;
 	}
 }
-/*
- * //아이디 중복체크 public int addIdCheck(String userID){ String SQL =
- * "select id from fun_member where id=?"; try {
- * pstmt=conn.prepareStatement(SQL); pstmt.setString(1, user.getUser_Id()); rs =
- * pstmt.executeQuery(); if(rs.next()){ if; }else{ result = 1; }
- * 
- * System.out.println("아이디 중복체크결과 : "+result); } catch (Exception e) { // TODO
- * Auto-generated catch block e.printStackTrace(); } finally { closeDB(); }
- * return result; }
- */
